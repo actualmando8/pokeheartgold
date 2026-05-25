807daa09f6777d01a3577ebbfee10a82ff2a9c2e
actualmando8 <armando.j.tor@gmail.com>
2026-05-25 19:06:08 -0400

    Fix function signatures in text_0205B4EC.h
    - Fixed sub_0205B514 params to void*
    - Fixed sub_0205B5EC params to void*
    - Fixed IsPrintFinished to take u32 param

--- include/text_0205B4EC.h
+++ include/text_0205B4EC.h
@@ -4,12 +4,12 @@
 #include "global.h"
 
 void sub_0205B4EC(void *taskMgr, u32 resetPrinters);
-u32 sub_0205B514(u32 param1, u32 param2, u32 frameType);
+u32 sub_0205B514(void *param1, void *param2, u32 frameType);
 void sub_0205B564(void *window, void *taskMgr);
 void sub_0205B5A8(void *window);
 void sub_0205B5B4(void *taskMgr, u32 windowId, u32 textData, u32 font);
-void sub_0205B5EC(void *taskMgr, u32 windowId, u32 textData, u32 font);
-u32 IsPrintFinished(void);
+u32 sub_0205B5EC(void *taskMgr, void *textData, u32 fontId, u32 textFrameDelay, BOOL canABSpeedUp, u32 a4);
+u32 IsPrintFinished(u32 textPrinterNum);
 u32 sub_0205B63C(u32 param1, u32 windowId, u32 sizeType, u32 frameType);
 void sub_0205B6A0(void *window, void *taskMgr, u32 frameId);
 
